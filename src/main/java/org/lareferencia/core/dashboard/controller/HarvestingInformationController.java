18a0d57c36e276b8da109f094a66f03a164cfaeb
Lautaro Matas <lmatas@gmail.com>
2023-05-29 19:13:25 +0200

    update harvesting information controller

--- src/main/java/org/lareferencia/core/dashboard/controller/HarvestingInformationController.java
+++ src/main/java/org/lareferencia/core/dashboard/controller/HarvestingInformationController.java
@@ -61,7 +61,6 @@ public class HarvestingInformationController {
 
 	private static Logger logger = LogManager.getLogger(HarvestingInformationController.class);
 
-
 	@Autowired
 	IHarvestingInformationService hService;
 
@@ -70,7 +69,7 @@ public class HarvestingInformationController {
 
 	@Autowired
 	ISecurityService securityService;
-	
+
 	@Autowired
 	DateHelper dateHelper;
 
@@ -81,28 +80,30 @@ public class HarvestingInformationController {
 	HttpEntity<Page<IHarvestingSource>> getSources(Pageable pageable) throws HarvesterInfoServiceException {
 
 		Page<IHarvestingSource> result = null;
-		
+
 		// if its a admin request show all sources
-		if ( securityService.isAdminRequest(request) ) {
+		if (securityService.isAdminRequest(request)) {
 			result = hService.listSources(pageable);
+		} else { // else get the groups of this user and return a filtered source list using
+					// groups as whitelist
+			result = hService.listSources(securityService.getRequestGroups(request), pageable);
 		}
-		else { // else get the groups of this user and return a filtered source list using groups as whitelist
-		    result = hService.listSources( securityService.getRequestGroups(request) , pageable);
-		}
-		 
+
 		return new ResponseEntity<Page<IHarvestingSource>>(result, HttpStatus.OK);
 	}
 
-//	@ApiOperation(value = "Returns a harvesting source info by source id")
-//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Returns a harvesting source info by source id") })
-//	@RequestMapping(value = "/by_id/{sourceID}", method = RequestMethod.GET)
-//	HttpEntity<IHarvestingSource> getSourceById(@PathVariable("sourceID") Long sourceID)
-//			throws HarvesterInfoServiceException {
-//
-//		IHarvestingSource result = hService.getSourceByID(sourceID);
-//
-//		return new ResponseEntity<IHarvestingSource>(result, HttpStatus.OK);
-//	}
+	// @ApiOperation(value = "Returns a harvesting source info by source id")
+	// @ApiResponses(value = { @ApiResponse(code = 200, message = "Returns a
+	// harvesting source info by source id") })
+	// @RequestMapping(value = "/by_id/{sourceID}", method = RequestMethod.GET)
+	// HttpEntity<IHarvestingSource> getSourceById(@PathVariable("sourceID") Long
+	// sourceID)
+	// throws HarvesterInfoServiceException {
+	//
+	// IHarvestingSource result = hService.getSourceByID(sourceID);
+	//
+	// return new ResponseEntity<IHarvestingSource>(result, HttpStatus.OK);
+	// }
 
 	@ApiOperation(value = "Returns a harvesting source info by source acronym")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Returns a harvesting source info by source acronym") })
@@ -115,17 +116,22 @@ public class HarvestingInformationController {
 		return new ResponseEntity<IHarvestingSource>(result, HttpStatus.OK);
 	}
 
-//	@ApiOperation(value = "Returns a harvesting source harvesting history  by source id")
-//	@ApiResponses(value = {
-//			@ApiResponse(code = 200, message = "Returns a harvesting source harvesting info by source id") })
-//	@RequestMapping(value = "/by_id/{sourceID}/history", method = RequestMethod.GET)
-//	HttpEntity<Page<IHarvestingResult>> getHarvestingHistoryById(@PathVariable("sourceID") Long sourceID,
-//			Pageable pageable) throws HarvesterInfoServiceException {
-//
-//		Page<IHarvestingResult> result = hService.getHarvestingHistoryBySourceID(sourceID, pageable);
-//
-//		return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
-//	}
+	// @ApiOperation(value = "Returns a harvesting source harvesting history by
+	// source id")
+	// @ApiResponses(value = {
+	// @ApiResponse(code = 200, message = "Returns a harvesting source harvesting
+	// info by source id") })
+	// @RequestMapping(value = "/by_id/{sourceID}/history", method =
+	// RequestMethod.GET)
+	// HttpEntity<Page<IHarvestingResult>>
+	// getHarvestingHistoryById(@PathVariable("sourceID") Long sourceID,
+	// Pageable pageable) throws HarvesterInfoServiceException {
+	//
+	// Page<IHarvestingResult> result =
+	// hService.getHarvestingHistoryBySourceID(sourceID, pageable);
+	//
+	// return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
+	// }
 
 	@ApiOperation(value = "Returns a harvesting source harvesting history by source acronym")
 	@ApiResponses(value = {
@@ -138,40 +144,63 @@ public class HarvestingInformationController {
 		logger.info("getHarvestingHistoryByAcronym: " + sourceAcronym);
 		logger.info("getHarvestingHistoryByAcronym: " + pageable);
 
-		Page<IHarvestingResult> result = hService.getHarvestingHistoryBySourceAcronym(sourceAcronym, pageable);
+		try {
+			Page<IHarvestingResult> result = hService.getHarvestingHistoryBySourceAcronym(sourceAcronym, pageable);
+			return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
 
-		return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
+		} catch (HarvesterInfoServiceException e) {
+			logger.error("getHarvestingHistoryByAcronym: " + e.getMessage());
+			return new ResponseEntity<Page<IHarvestingResult>>(HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			logger.error("getHarvestingHistoryByAcronym: " + e.getMessage());
+			return new ResponseEntity<Page<IHarvestingResult>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
-	
-	
+
 	@ApiOperation(value = "Returns a harvesting source harvesting history within a time interval by source acronym")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "Returns a harvesting source harvesting info for a given time interval by source acronym") })
 	@RequestMapping(value = "/{sourceAcronym}/history/{startDate}/{endDate}", method = RequestMethod.GET)
 	HttpEntity<Page<IHarvestingResult>> getHarvestingHistoryByAcronymAndDate(
-			@PathVariable("sourceAcronym") String sourceAcronym, @PathVariable("startDate") String fromDate, @PathVariable("endDate") String toDate, Pageable pageable)
-			throws HarvesterInfoServiceException {
-    
-	// converts from dates to localdatetime at the start and end of the given dates	
-    LocalDateTime startDate =  dateHelper.parseDate(fromDate).toLocalDate().atStartOfDay();
-    LocalDateTime endDate = dateHelper.parseDate(toDate).toLocalDate().atTime(LocalTime.MAX);
-    
-    Page<IHarvestingResult> result = hService.getHarvestingHistoryBySourceAcronym(sourceAcronym, startDate, endDate, pageable);
+			@PathVariable("sourceAcronym") String sourceAcronym, @PathVariable("startDate") String fromDate,
+			@PathVariable("endDate") String toDate, Pageable pageable)
+	{
 
-		return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
+		// converts from dates to localdatetime at the start and end of the given dates
+		LocalDateTime startDate = dateHelper.parseDate(fromDate).toLocalDate().atStartOfDay();
+		LocalDateTime endDate = dateHelper.parseDate(toDate).toLocalDate().atTime(LocalTime.MAX);
+
+		try {
+			Page<IHarvestingResult> result = hService.getHarvestingHistoryBySourceAcronym(sourceAcronym, startDate,
+					endDate, pageable);
+			return new ResponseEntity<Page<IHarvestingResult>>(result, HttpStatus.OK);
+
+		} catch (HarvesterInfoServiceException e) {
+			logger.error("getHarvestingHistoryByAcronymAndDate: " + e.getMessage());
+			return new ResponseEntity<Page<IHarvestingResult>>(HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			logger.error("getHarvestingHistoryByAcronymAndDate: " + e.getMessage());
+			return new ResponseEntity<Page<IHarvestingResult>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 
-//	@ApiOperation(value = "Returns a harvesting source last good known harvesting by source id")
-//	@ApiResponses(value = {
-//			@ApiResponse(code = 200, message = "Returns a harvesting source last good known harvesting by source id") })
-//	@RequestMapping(value = "/by_id/{sourceID}/lkg", method = RequestMethod.GET)
-//	HttpEntity<IHarvestingResult> getLGKSnapshotBySourceId(@PathVariable("sourceID") Long sourceID)
-//			throws HarvesterInfoServiceException {
-//
-//		IHarvestingResult result = hService.getLastKnownGoodHarvestingBySourceID(sourceID);
-//
-//		return new ResponseEntity<IHarvestingResult>(result, HttpStatus.OK);
-//	}
+	// @ApiOperation(value = "Returns a harvesting source last good known harvesting
+	// by source id")
+	// @ApiResponses(value = {
+	// @ApiResponse(code = 200, message = "Returns a harvesting source last good
+	// known harvesting by source id") })
+	// @RequestMapping(value = "/by_id/{sourceID}/lkg", method = RequestMethod.GET)
+	// HttpEntity<IHarvestingResult>
+	// getLGKSnapshotBySourceId(@PathVariable("sourceID") Long sourceID)
+	// throws HarvesterInfoServiceException {
+	//
+	// IHarvestingResult result =
+	// hService.getLastKnownGoodHarvestingBySourceID(sourceID);
+	//
+	// return new ResponseEntity<IHarvestingResult>(result, HttpStatus.OK);
+	// }
 
 	@ApiOperation(value = "Returns a harvesting source last good known harvesting by source acronym")
 	@ApiResponses(value = {
@@ -184,37 +213,40 @@ public class HarvestingInformationController {
 
 		return new ResponseEntity<IHarvestingResult>(result, HttpStatus.OK);
 	}
-	
+
 	@ApiOperation(value = "Returns the metadata for a record by id and source acronym")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "Returns the metadata for a record by id and source acronym") })
 	@RequestMapping(value = "/{sourceAcronym}/record/{recordID}", method = RequestMethod.GET)
-	HttpEntity<String> getRecordMetadataByID(@PathVariable("sourceAcronym") String sourceAcronym, @PathVariable("recordID") Long recordID) throws HarvesterInfoServiceException {
-      
-      String result = hService.getRecordMetadataByRecordIDAndSourceAcronym(sourceAcronym, recordID);
-      
-      return new ResponseEntity<String>(result, HttpStatus.OK);
+	HttpEntity<String> getRecordMetadataByID(@PathVariable("sourceAcronym") String sourceAcronym,
+			@PathVariable("recordID") Long recordID) throws HarvesterInfoServiceException {
+
+		String result = hService.getRecordMetadataByRecordIDAndSourceAcronym(sourceAcronym, recordID);
+
+		return new ResponseEntity<String>(result, HttpStatus.OK);
 
 	}
 
-//	private Page<IHarvestingSource> filterByUserPermissions(Page<IHarvestingSource> list, Pageable pageable) {
-//
-//		KeycloakAuthorization authz = new KeycloakAuthorization(getKeycloakSecurityContext());
-//		List<String> userGroups = authz.getGroups();
-//		List<IHarvestingSource> sources = list.getContent();
-//		List<IHarvestingSource> filtered = new ArrayList<IHarvestingSource>();
-//
-//		if (authz.hasRole(adminRole)) {
-//			filtered = sources;
-//		} else {
-//			for (IHarvestingSource source : sources) {
-//				if (userGroups.contains(source.getAcronym())) {
-//					filtered.add(source);
-//				}
-//			}
-//		}
-//
-//		return new PageImpl<IHarvestingSource>(filtered, pageable, filtered.size());
-//	}
+	// private Page<IHarvestingSource>
+	// filterByUserPermissions(Page<IHarvestingSource> list, Pageable pageable) {
+	//
+	// KeycloakAuthorization authz = new
+	// KeycloakAuthorization(getKeycloakSecurityContext());
+	// List<String> userGroups = authz.getGroups();
+	// List<IHarvestingSource> sources = list.getContent();
+	// List<IHarvestingSource> filtered = new ArrayList<IHarvestingSource>();
+	//
+	// if (authz.hasRole(adminRole)) {
+	// filtered = sources;
+	// } else {
+	// for (IHarvestingSource source : sources) {
+	// if (userGroups.contains(source.getAcronym())) {
+	// filtered.add(source);
+	// }
+	// }
+	// }
+	//
+	// return new PageImpl<IHarvestingSource>(filtered, pageable, filtered.size());
+	// }
 
 }
