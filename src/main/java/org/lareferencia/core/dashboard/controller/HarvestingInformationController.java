f12022c435a9c312ca698574d53471ad38b23227
Lautaro Matas <lmatas@gmail.com>
2025-08-25 12:55:05 +0200

    Update HarvestingInformationController.java

--- src/main/java/org/lareferencia/core/dashboard/controller/HarvestingInformationController.java
+++ src/main/java/org/lareferencia/core/dashboard/controller/HarvestingInformationController.java
@@ -217,6 +217,20 @@ public class HarvestingInformationController {
 	}
 
 
+	@Deprecated
+	@ApiOperation(value = "Returns the metadata for a record by id and source acronym")
+	@ApiResponses(value = {
+	@ApiResponse(code = 200, message = "Returns the metadata for a record by id and source acronym") })
+	@RequestMapping(value = "/{sourceAcronym}/record/{recordID}", method = RequestMethod.GET)
+	HttpEntity<String> getRecordMetadataByID(@PathVariable("sourceAcronym") String sourceAcronym,
+			@PathVariable("recordID") Long recordID) throws HarvesterInfoServiceException {
+
+		String result = hService.getRecordMetadataByRecordIDAndSourceAcronym(sourceAcronym, recordID);
+
+		return new ResponseEntity<String>(result, HttpStatus.OK);
+
+	}
+	
 	@ApiOperation(value = "Returns the metadata for a record by snapshot id and identifier")
 	@ApiResponses(value = {
 	@ApiResponse(code = 200, message = "Returns the metadata for a record by snapshot id and identifier") })
